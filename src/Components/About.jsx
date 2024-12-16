cad4e3fb7446283cb3a75beb3cf3b70b7eb2c6d6
Hafiz Muhammad Furqan <hafizm.furqan123@gmail.com>
2024-12-16 13:11:35 +0500

    merge conflicts

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,35 +4,6 @@ const About = ({ aboutRef }) => {
       ref={aboutRef}
       className="min-h-[calc(100svh-80px)] w-full flex items-center justify-center lg:pt-16 mb-20 lg:px-14 px-4 "
     >
-<<<<<<< HEAD
-      <div className="lg:w-1/2 w-full lg:h-full  flex items-center justify-center ">
-        <img
-          src="/about-me.png"
-          alt="about"
-          className="lg:size-[570px] 2xl:size-[740px] size-[400px] object-contain"
-        />
-      </div>
-      <div className="lg:w-1/2 w-full lg:h-full flex  flex-col items-center lg:items-start justify-center  gap-6 lg:pr-[150px] ">
-        <h3 className="text-lg font-semibold text-center lg:text-start 2xl:text-2xl 2xl:font-bold">
-          About
-        </h3>
-        <h1 className="lg:text-5xl text-3xl font-bold text-center lg:text-start 2xl:text-6xl 2xl:font-extrabold">
-          About Me
-        </h1>
-        <p className="text-base font-medium tracking-wide text-center lg:text-start 2xl:text-lg 2xl:font-semibold">
-          I am a skilled web developer with expertise in React, Firebase, and
-          Redux Toolkit. He has a strong focus on building user-friendly
-          interfaces and integrating APIs to deliver high-quality web
-          applications. His work reflects creativity and a commitment to solving
-          complex challenges effectively.
-        </p>
-        <p className="text-base font-medium tracking-wide text-center lg:text-start 2xl:text-lg 2xl:font-semibold">
-          Currently, I am expanding my skills into backend development with
-          frameworks like Node.js and Next.js. With a keen interest in AI-driven
-          solutions, I am passionate about creating impactful projects that
-          blend innovation with functionality.
-        </p>
-=======
       <div className="h-full w-full max-w-[1600px] flex flex-col  lg:flex-row items-center justify-center gap-20 lg:gap-0">
         <div className="lg:w-1/2 w-full lg:h-full  flex items-center justify-center ">
           <img
@@ -62,7 +33,6 @@ const About = ({ aboutRef }) => {
             projects that blend innovation with functionality.
           </p>
         </div>
->>>>>>> 2aef984
       </div>
     </div>
   );
